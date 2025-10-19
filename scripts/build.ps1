0de5185aebb9c7e776824299874952765fb0bbee
vlad <vlad19972017@gmail.com>
2025-10-19 13:07:58 +0300

    Update scripts part 2

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,20 @@
 $ProjectName = Split-Path (Split-Path $PSScriptRoot -Parent) -Leaf
-Write-Host "Building project: $ProjectName"
+
+Write-Output "::group::🧱 Building project"
+
+Write-Host "Building project name: $ProjectName"
+Push-Location $PSScriptRoot
+
+try
+{
+    dotnet restore "$ProjectName.sln"
+    dotnet build "$ProjectName.sln" --configuration Release
+    Write-Host "`e[32m✅ Success:`e[0m Build completed successfully!"
+}
+finally
+{
+    Write-Host "`e[31m❌ Error:`e[0m Something went wrong!"
+    Pop-Location
+}
+
+Write-Output "::endgroup::"
